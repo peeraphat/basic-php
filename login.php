4d46527112ee159f81f9a1eddb608ce1f268c205
unknown <peeraphat.thong@gmail.com>
2018-01-04 16:47:49 +0700

    little change

--- login.php
+++ login.php
@@ -1,8 +1,16 @@
 <?php 
     session_start();
+?>
+<!-- <script src="libs/bt4/js/bootstrap.min.js"></script>
+<script src="libs/js/jquery-3.2.1.min.js"></script>
+<script src="libs/js/popper.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<?php
     require_once('config/database.php');
     if(isset($_SESSION['username'])){
-        header('Location: home.php');
+        header('Location: dashboard.php');
     }
     if(isset($_GET['method'])) {
         if($_GET['method'] =="login") {
@@ -17,19 +25,45 @@
                     echo "pass";
                     $_SESSION['username'] = $result["member_username"];
                     $_SESSION['role'] = $results["member_role"];
-                    header('Location: home.php');
+                    header('Location: dashboard.php');
                 } else {
-                    echo "wrong!";
+                    echo "<div class='alert alert-danger' role='alert'>Something wrong!</div>";
                 }
             } else {
-                echo 'false';
+                echo "<div class='alert alert-danger' role='alert'>Something wrong!</div>";
             }
         }
     }
 ?>
-<h1>Login page.</h1>
-<form method="post" action="login.php?method=login">
-    <input type="text" name="username" id="username">
-    <input type="password" name="pwd" id="pwd">
-    <input type="submit" value="Login">
-</form>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Login</title>
+    <!-- Bootstrap core CSS -->
+    <link href="libs/bt4/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="libs/style/login.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="container">
+      <form class="form-signin" method="post" action="login.php?method=login">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="text" name="username" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="pwd" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+    </div> <!-- /container -->
+  </body>
+</html>
--- register.php
+++ register.php
@@ -5,16 +5,30 @@
         if($_GET['method'] == "register") {
             $username = $_POST['username'];
             $password = $_POST['password'];
+            $name = $_POST['name'];
+            $lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            $gender = $_POST['gender'];
             $password = md5($password);
 
-            $sql = "INSERT INTO table_member (member_username, member_password) Values ('$username', '$password')";
-            $result = $dbh->exec($sql);
-            if($result) {
-                $_SESSION['username'] = $username;
-                $_SESSION['role'] = '1';
-                header('Location: home.php');
+            $sqlCheckUser = "SELECT COUNT(member_username) FROM table_member WHERE member_username='$username'";
+            $resCheckUser = $dbh->query($sqlCheckUser);
+            $countCheckUser = $resCheckUser->fetchColumn();
+            if($countCheckUser == 0) {
+                $sql = "INSERT INTO table_member (member_username, member_password, member_name, member_lastName, member_email, member_gender) Values ('$username', '$password', '$name', '$lastName', '$email', '$gender')";
+                $result = $dbh->exec($sql);
+                if($result) {
+                    $_SESSION['username'] = $username;
+                    $_SESSION['role'] = '1';
+                    header('Location: dashboard.php');
+                } else {
+                    echo "some thing error";
+                }
             } else {
-                echo "some thing error";
+                echo "Sorry!, This username is exist.";
+                echo "<br />";
+                echo "<a href='javascript:history.go(-1)'>Back</a>";
+                exit();
             }
         }
     }
@@ -30,8 +44,14 @@
 <body>
     <h1>Register page.</h1>
     <form action="register.php?method=register" method="post">
-        <input type="text" name="username" id="username">
-        <input type="password" name="password" id="password">
+        <input type="text" name="username" placeholder="username" id="username"> <br />
+        <input type="password" name="password" placeholder="password" id="password"> <br />
+        <input type="text" name="name" placeholder="name" id="name"> <br />
+        <input type="text" name="lastName" placeholder="lastName" id="lastName"> <br />
+        <input type="text" name="email" placeholder="email" id="email"> <br />
+        <input type="radio" name="gender" placeholder="gender" id="gender" value="m"> Male
+        <input type="radio" name="gender" placeholder="gender" id="gender" value="f"> Female
+        <br />
         <input type="submit" value="Submit">
     </form>
 </body>
